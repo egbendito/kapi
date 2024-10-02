be3788fe164635d519960b3f396088f272b8352f
reginakavive <reginakavive@gmail.com>
2024-10-02 11:26:21 +0000

    update

--- other/api/app.R
+++ other/api/app.R
@@ -2,6 +2,6 @@
 library(plumber)
 # 'plumber.R' is the location of the file shown above
 pr("other/api/plumber.R") %>%
-  pr_run(port=8008)
+  pr_run(port=8080)
 
 # ngrok http http://127.0.0.1:8008
